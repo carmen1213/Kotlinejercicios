32b398a799c86986a98ab6c6e39fd3483af9d811
carmen1213 <74176423+carmen1213@users.noreply.github.com>
2021-10-20 14:09:55 +0200

    hechos en clase

--- src/Ejercicioclase3_1.kt
+++ src/Ejercicioclase3_1.kt
@@ -1,7 +1,8 @@
 fun main() {
+    var sumaarray : Int = 0
     var sumapares : Int = 0
-    var sumaimpares : Int = 0
-    val Arraypromedio = " "
+    var restaimpares : Int = 0
+    var promedio : Int = 0
     val Arraynumeros = mutableListOf<Int>()
     val ArrayNumerosimpares= mutableListOf<Int>()
     val ArrayNumerospares= mutableListOf<Int>()
@@ -11,10 +12,44 @@ fun main() {
         Arraynumeros.add(numeros)
     }
     Arraynumeros.sorted()
+    if (Arraynumeros.contains(10)) {
+        print("En este Array esta el 10" + "\n")
+    }
     for (i in 0..9) {
+        if (Arraynumeros[i] % 2 == 0) {
+            ArrayNumerospares.add(Arraynumeros[i])
+        } else {
+            ArrayNumerosimpares.add(Arraynumeros[i])
+        }
 //sumapares
     }
 
+    println("Este es el array de 10 numeros introducidos por el usuario : =  " + Arraynumeros+ "\n")
+    println("Este es el array de numeros impares :  = " +  ArrayNumerosimpares+ "\n")
+    println("Este es el array de numeros pares :  = " +ArrayNumerospares+ "\n")
+    for (i in 0..ArrayNumerospares.size-2){
+        sumapares = (sumapares + ArrayNumerospares[i])
+
+    }
+    print("La suma de los numeros pares es : =  " +sumapares+ "\n")
+
+    for (i in 0..ArrayNumerosimpares.size-2){
+        restaimpares = (restaimpares - ArrayNumerosimpares[i])
+
+    }
+
+    print("La resta de los numeros impares es : =  " +  restaimpares+ "\n")
+
+
+    for (i in 0..Arraynumeros.size-2){
+        sumaarray = (sumaarray + Arraynumeros[i])
+    }
+    print("La suma de este array es : =  " + sumaarray+ "\n")
+
+
+    promedio = (sumaarray / 10)
+
+    print("El promedio de este array es : =  "+promedio + "\n")
 
 }
 
--- src/Ejercicioclase4.kt
+++ src/Ejercicioclase4.kt
@@ -1,20 +1,34 @@
-fun main(){
-var Mapaempleados: Map<String,Int> = mapOf()
-    var salario =  " "
+fun main() {
+    var Mapaempleados: MutableMap<String, Int> = mutableMapOf()
+
+    var nombres: String = " "
+    var horas: Int = 0
+    val constante: Double = 37.76
+    val Arrayhoras = mutableListOf<Int>()
     for (i in 0..9) {
         println("Escribe 10 nombres de empleados ")
-        var nombres = readLine().toString()
+        nombres = readLine()!!.toString()
         println(" Y las respectivas horas trabajadas ")
-        var horas = readLine()!!.toInt()
-        Mapaempleados= mutableMapOf(nombres to horas)
-       // salario = (horas * "37,76")
-        println(Mapaempleados)
-
+        horas = readLine()!!.toInt()
+        Mapaempleados[nombres]=horas
     }
+    Mapaempleados.forEach {
+        var salario = it.value * 37.76
+        if (salario <= 1600.0) {
+            println(it.key + " cobra : " + salario + "")
+        }else{
+            println("Revisar horas extras del empleado  " + it.key)
+        }
+    }
+    Arrayhoras.add(horas)
 
+
+
+    println(Mapaempleados)
 
 
 
 }
+
 
 
